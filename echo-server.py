2e92431c6b5d20e251852d856e825cafff4ffb9b
Channon Zuo <channon.z2020@gmail.com>
2023-07-15 14:24:46 -0700

    looping logic to send segments and receive response

--- echo-server.py
+++ echo-server.py
@@ -1,18 +1,30 @@
 # Server echoes client's messages
 # https://realpython.com/python-sockets/
 import socket
+HOST = socket.gethostname()
+IP = socket.gethostbyname(HOST)
 
-HOST = "127.0.0.1"  # replace w local IP of server
 PORT = 50000
 PACKET_SIZE = 1024
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((IP, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        print(f"Success! Connected by {addr}")
         while True:
             data = conn.recv(PACKET_SIZE)
             if not data:
                 break
-            conn.sendall(data)  # echoes back the client data
+            data_decode = data.decode("utf-8")
+            data_decode += " 1"
+            data_encode = data_decode.encode('utf-8')
+            conn.sendall(data_encode)  # echoes back the client data
+
+            data = conn.recv(PACKET_SIZE)
+            if not data:
+                break
+            data_decode = data.decode("utf-8")
+            data_decode = int(data_decode)
+            data_decode += 1
+            conn.sendall(b"Next: " + str(data_decode).encode('utf-8'))
