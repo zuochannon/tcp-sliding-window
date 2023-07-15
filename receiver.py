3469382eaf35c1dd377f7d358ff6b37cd9eb3b27
Channon Zuo <channon.z2020@gmail.com>
2023-07-15 13:41:36 -0700

    host on local desktop for now

--- receiver.py
+++ receiver.py
@@ -1,6 +1,5 @@
 # server code
 import socket
-import sys
 
 PORT_NUMBER = 6969
 SIZE = 1024
@@ -10,9 +9,9 @@ mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.bind((hostName, PORT_NUMBER))
 mySocket.listen(5)  # queue of 5
 
-print("Test server listening on port {0}\n".format(PORT_NUMBER))
+print(f"Test server listening on port {PORT_NUMBER}")
 
 while True:
     data, addr = mySocket.accept()
-    print(f"Connection from {addr} has been established")
+    print(f"Succesful connection from {addr} has been established")
     data.send(bytes("Welcome", "utf-8"))  # prints to sender console
--- sender.py
+++ sender.py
@@ -5,11 +5,11 @@ SIZE = 1024
 # print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
 
 hostName = socket.gethostname()
-
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.connect((hostName, PORT_NUMBER))
-
+mySocket.send(bytes("Network", "utf-8"))
 msg = mySocket.recv(SIZE)
 print(msg.decode("utf-8"))
+mySocket.close()
 # while True:
 #         mySocket.sendto('cool',(SERVER_IP,PORT_NUMBER))
